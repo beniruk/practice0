6e65e0ef09b49351f76766ef83a8b907f1533d90
beniruk <beniruk@gmail.com>
2020-04-27 00:44:23 +0300

    testing with ci/cd

---
+++ authenti/authentic/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "author/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "author/user.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "author/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "author/login.html", {"message": "Logged out."})
